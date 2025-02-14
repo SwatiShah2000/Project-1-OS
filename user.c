fbca09b1aef02dfce68e41c8a5df2e4d11c93270
SwatiShah2000 <swastikashah2000@gmail.com>
2025-02-13 21:13:11 -0600

    Added debug messages, expected output, and output files

--- user.c
+++ user.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 
 int main() {
-    printf("This is a user process with PID: %d\n", getpid());
+    printf("DEBUG: Process started\n");
+    fflush(stdout);
 
+    printf("This is a user process with PID: %d\n", getpid());
     sleep(rand() % 5 + 1);
 
     return 0;
